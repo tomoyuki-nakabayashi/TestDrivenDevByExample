087f24c9ce1fab59681b49c51bcf0f89161f9e44
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-05-01 16:50:38 +0900

    Section 8. pp.52 Makin' Objects. #1

--- CppTemplate/test/money_test.cpp
+++ CppTemplate/test/money_test.cpp
@@ -15,22 +15,22 @@ class MoneyTest : public ::testing::Test {
 
 TEST_F(MoneyTest, Multiplication) {
   constexpr Money<Dollar> five{5};
-  static_assert((five*2) == Dollar{10}, "product must be 10.");
-  static_assert((five*3) == Dollar{15}, "product must be 15.");
+  static_assert((five*2) == Money<Dollar>{10}, "product must be 10.");
+  static_assert((five*3) == Money<Dollar>{15}, "product must be 15.");
 }
 
 TEST_F(MoneyTest, Equality) {
-  static_assert(Dollar{5} == Dollar{5}, "Two objects must be same.");
-  static_assert(!(Dollar{5} == Dollar{6}), "Two objects must be different.");
-  static_assert(Franc{5} == Franc{5}, "Two objects must be same.");
-  static_assert(!(Franc{5} == Franc{6}), "Two objects must be different.");
-  static_assert(!(Franc{5} == Dollar{5}), "Two objects must be different.");
+  static_assert(Money<Dollar>{5} == Money<Dollar>{5}, "Two objects must be same.");
+  static_assert(!(Money<Dollar>{5} == Money<Dollar>{6}), "Two objects must be different.");
+  static_assert(Money<Franc>{5} == Money<Franc>{5}, "Two objects must be same.");
+  static_assert(!(Money<Franc>{5} == Money<Franc>{6}), "Two objects must be different.");
+  static_assert(!(Money<Franc>{5} == Money<Dollar>{5}), "Two objects must be different.");
 }
 
 TEST_F(MoneyTest, FrancMultiplication) {
   constexpr Money<Franc> five{5};
-  static_assert((five*2) == Franc{10}, "product must be 10.");
-  static_assert((five*3) == Franc{15}, "product must be 15.");
+  static_assert((five*2) == Money<Franc>{10}, "product must be 10.");
+  static_assert((five*3) == Money<Franc>{15}, "product must be 15.");
 }
 
 }  // namespace money_test
